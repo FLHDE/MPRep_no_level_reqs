f5264a54a3ed5ac2b85b94b31a388d8ddb0be2ae
C.S. Melis <cees3331x00@gmail.com>
2025-04-16 00:30:10 +0200

    Remove reputation requirement feature

--- MPRep.cpp
+++ MPRep.cpp
@@ -38,18 +38,6 @@
 #define ADDR_STATS    ((PDWORD)(0x53c3e1+1))
 #define ADDR_ENTER    ((PDWORD)(0x558986+1))
 
-#define ADDR_LEVEL1   ((PBYTE)0x47fcdf) 	// equipment/goods
-#define ADDR_LEVEL2   ((PBYTE)0x480499+1)
-#define ADDR_LEVEL3   ((PBYTE)0x482e95+1)
-#define ADDR_LEVEL4   ((PBYTE)0x4b850d+1)	// ships
-#define ADDR_LEVEL5   ((PBYTE)0x4b948d+1)
-
-#define ADDR_SHIP1    ((PBYTE)0x4b946b+1)
-#define ADDR_SHIP2    ((PBYTE)0x4b9472+1)
-#define ADDR_SHIP3    ((PBYTE)0x4b9495+1)
-#define ADDR_SHIP4    ((PBYTE)0x4b94aa)
-#define ADDR_SHIP5    ((PBYTE)0x4b94b6+2)
-
 
 DWORD dummy;
 #define ProtectX( addr, size ) \
@@ -65,7 +53,6 @@ DWORD dummy;
 
 DWORD GetGroupFeelingsTowards_Org;
 DWORD ReceivePlayerStats_Org;
-DWORD BaseEnter_Org;
 
 
 struct Rep
@@ -136,19 +123,6 @@ void ReceivePlayerStats_Hook()
 }
 
 
-NAKED
-void BaseEnter_Hook()
-{
-  __asm {
-	push	ecx
-	call	RequestPlayerStats
-	pop	ecx
-	jmp	BaseEnter_Org
-	align	16
-  }
-}
-
-
 int fac_cmp( const void* key, const void* elem )
 {
   return (UINT)key - ((Rep*)elem)->faction;
@@ -192,31 +166,9 @@ void Patch()
   ProtectX( ADDR_STATS,    4 );
   ProtectX( ADDR_ENTER,    4 );
 
-  ProtectX( ADDR_LEVEL1,   7 );
-  ProtectX( ADDR_LEVEL2,   1 );
-  ProtectX( ADDR_LEVEL3,   1 );
-  ProtectX( ADDR_LEVEL4,   1 );
-  ProtectX( ADDR_LEVEL5,   1 );
-
-  //ProtectX( ADDR_SHIP1,  1 );
-  //ProtectX( ADDR_SHIP2,  1 );
-
   GetGroupFeelingsTowards_Org = *ADDR_FEELINGS;
   *ADDR_FEELINGS = (DWORD)GetGroupFeelingsTowards_Hook;
   NEWOFS( ADDR_STATS, ReceivePlayerStats_Hook, ReceivePlayerStats_Org );
-  NEWOFS( ADDR_ENTER, BaseEnter_Hook, BaseEnter_Org );
-
-  memcpy( ADDR_LEVEL1, "\xe8\x0c\x59\x04\x00"   // do both the SP
-		       "\xeb\x13", 7 );         // and MP code
-  *ADDR_LEVEL2 = *ADDR_LEVEL3 = *ADDR_LEVEL4 = *ADDR_LEVEL5 = 0; // ignore MP test
-
-  // Select the right ship when reputation changes order.
-  *ADDR_SHIP1 = 0x3f;
-  *ADDR_SHIP2 = 0x38;
-  *ADDR_SHIP3 += 2;
-  *ADDR_SHIP5 -= 4;
-  memmove( ADDR_SHIP4 + 4, ADDR_SHIP4, 0x12 );
-  memcpy( ADDR_SHIP4, "\xeb\x02\xdd\xd8", 4 );  // jmp $+2; fstp st
 
   SinglePlayer = (bool*)((DWORD)GetModuleHandle( "common.dll" ) - 0x6260000 + 0x63ed17c);
 }
